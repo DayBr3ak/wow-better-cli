a8437022119f16636d9d977f118018df179e8d5d
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2016-07-20 16:23:47 +0200

    add a test for extracting zip addon

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,9 @@
 // test.js
 'use strict';
 const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const temp = require('temp').track();
 
 const chai = require('chai');
 const assert = chai.assert;
@@ -11,6 +14,25 @@ const IsNumeric = (n) => {
 }
 
 const testTimeout = 20*1000;
+
+const makeTmpWowFolder = (cb) => {
+    temp.mkdir('wowfolder', function(err, wowFolder) {
+        if (err) {
+            return cb(err)
+        }
+
+        let interfaceDir = path.join(wowFolder, 'Interface')
+        mkdirp(interfaceDir, (err) => {
+            should.not.exist(err)
+            let addonsDir = path.join(interfaceDir, 'AddOns')
+            mkdirp(addonsDir, (err) => {
+                should.not.exist(err)
+
+                cb(null, addonsDir);
+            })
+        })
+    })
+}
 
 describe('Curse', function() {
     const curse = require('../lib/curse.js');
@@ -30,7 +52,7 @@ describe('Curse', function() {
 
                 IsNumeric(version).should.equal(true);
                 done()
-            }) 
+            })
         })
     })
 });
@@ -50,8 +72,35 @@ describe('downloader', function() {
                         done();
                     });
                 })
-            }) 
+            })
         })
     })
 });
+
+describe('download wow addon into wow folder', function() {
+    const curse = require('../lib/curse.js');
+    const downloader = require('../lib/downloader.js');
+    describe('with curse', function () {
+        it('should download an addon, extract it, and place it into the wow interface addons folder', function(done) {
+            this.timeout(testTimeout);
+            let downloadAddon = (output) => {
+                curse.getDownloadURL('Ace3', null, (err, url, version) => {
+                    downloader.downloadAddonToTempFile(url, (err, path) => {
+                        downloader.extractZip(path, output, (err, folders) => {
+                            should.not.exist(err);
+                            should.exist(folders);
+                            done()
+
+                        })
+                    });
+                })
+            };
+            makeTmpWowFolder(function(err, addonPath) {
+                should.not.exist(err);
+                should.exist(addonPath);
+                downloadAddon(addonPath);
+            })
+        })
+    })
+})
 
