ad7e3e0ae7fe1f6d693bc17bf1957dd9fb36e445
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2016-07-20 18:08:39 +0200

    working on the Wow class (install addons etc)

--- tests/test.js
+++ tests/test.js
@@ -13,6 +13,11 @@ const IsNumeric = (n) => {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+const curse = require('../lib/curse.js');
+const downloader = require('../lib/downloader.js');
+const Wow = require('../lib/index.js');
+
+
 const testTimeout = 20*1000;
 
 const makeTmpWowFolder = (cb) => {
@@ -28,14 +33,13 @@ const makeTmpWowFolder = (cb) => {
             mkdirp(addonsDir, (err) => {
                 should.not.exist(err)
 
-                cb(null, addonsDir);
+                cb(null, wowFolder);
             })
         })
     })
 }
 
 describe('Curse', function() {
-    const curse = require('../lib/curse.js');
     describe('getDownloadUrl()', function() {
 
         it('should return the download url of an addon', function(done) {
@@ -54,36 +58,15 @@ describe('Curse', function() {
                 done()
             })
         })
-
-        it('promise version: should return the download url of an addon', function(done) {
-            this.timeout(testTimeout)
-            curse.getDownloadURL('Ace3', null)
-                .then((infos) => {
-                    should.exist(infos);
-                    let {url, version} = infos;
-                    should.exist(url);
-                    should.exist(version);
-                    const zipRegex = /\.zip/;
-                    let hasZip = zipRegex.exec(url);
-
-                    should.not.equal(null, hasZip);
-                    should.not.equal(null, version);
-
-                    IsNumeric(version).should.equal(true);
-                    done()
-                })
-        })
     })
 });
 
 describe('downloader', function() {
-    const curse = require('../lib/curse.js');
-    const downloader = require('../lib/downloader.js');
-    describe('downloadAddonToTempFile()', function() {
+    describe('downloadZipToTempFile()', function() {
         it('should download the zip of an addon', function(done) {
             this.timeout(testTimeout);
             curse.getDownloadURL('Ace3', null, (err, url, version) => {
-                downloader.downloadAddonToTempFile(url, (err, path) => {
+                downloader.downloadZipToTempFile(url, (err, path) => {
                     should.not.exist(err);
                     should.exist(path);
                     fs.access(path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
@@ -97,27 +80,19 @@ describe('downloader', function() {
 });
 
 describe('download wow addon into wow folder', function() {
-    const curse = require('../lib/curse.js');
-    const downloader = require('../lib/downloader.js');
     describe('with curse', function () {
         it('should download an addon, extract it, and place it into the wow interface addons folder', function(done) {
             this.timeout(testTimeout);
-            let downloadAddon = (output) => {
-                curse.getDownloadURL('Ace3', null, (err, url, version) => {
-                    downloader.downloadAddonToTempFile(url, (err, path) => {
-                        downloader.extractZip(path, output, (err, folders) => {
-                            should.not.exist(err);
-                            should.exist(folders);
-                            done()
-
-                        })
-                    });
-                })
-            };
-            makeTmpWowFolder(function(err, addonPath) {
+            makeTmpWowFolder(function(err, wowPath) {
                 should.not.exist(err);
-                should.exist(addonPath);
-                downloadAddon(addonPath);
+                should.exist(wowPath);
+
+                let wow = new Wow(wowPath);
+                wow.install('curse', 'Ace3', null, (err, folders) => {
+                    should.not.exist(err);
+                    should.exist(folders);
+                    done()
+                })
             })
         })
     })
