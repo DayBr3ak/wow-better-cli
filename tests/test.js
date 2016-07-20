9b0d70da63e989641e417a8566c1a5e16cfd4b8c
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2016-07-20 15:29:23 +0200

    add test for dl to tempfile

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 // test.js
 'use strict';
+const fs = require('fs');
+
 const chai = require('chai');
 const assert = chai.assert;
 const should = chai.should();
@@ -8,11 +10,14 @@ const IsNumeric = (n) => {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+const testTimeout = 20*1000;
 
-describe('Curse', () => {
+describe('Curse', function() {
     const curse = require('../lib/curse.js');
-    describe('getDownloadUrl()', () => {
-        it('should return the download url of an addon', (done) => {
+    describe('getDownloadUrl()', function() {
+
+        it('should return the download url of an addon', function(done) {
+            this.timeout(testTimeout)
             curse.getDownloadURL('Ace3', null, (err, url, version) => {
                 should.not.exist(err);
                 should.exist(url);
@@ -30,23 +35,21 @@ describe('Curse', () => {
     })
 });
 
-describe('downloader', () => {
+describe('downloader', function() {
     const curse = require('../lib/curse.js');
-    const downloader = require('../downloader.js');
-    describe('getDownloadUrl()', () => {
-        it('should return the download url of an addon', (done) => {
+    const downloader = require('../lib/downloader.js');
+    describe('downloadAddonToTempFile()', function() {
+        it('should download the zip of an addon', function(done) {
+            this.timeout(testTimeout);
             curse.getDownloadURL('Ace3', null, (err, url, version) => {
-                should.not.exist(err);
-                should.exist(url);
-                should.exist(version);
-                const zipRegex = /\.zip/;
-                let hasZip = zipRegex.exec(url);
-
-                should.not.equal(null, hasZip);
-                should.not.equal(null, version);
-
-                IsNumeric(version).should.equal(true);
-                done()
+                downloader.downloadAddonToTempFile(url, (err, path) => {
+                    should.not.exist(err);
+                    should.exist(path);
+                    fs.access(path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+                        should.not.exist(err);
+                        done();
+                    });
+                })
             }) 
         })
     })
