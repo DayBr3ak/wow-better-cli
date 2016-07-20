e329dba3d98f40bc6f3a64c2be6605335f857936
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2016-07-20 14:09:39 +0200

    test + fixed crap curse url

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,54 @@
 // test.js
+'use strict';
+const chai = require('chai');
+const assert = chai.assert;
+const should = chai.should();
 
-var assert = require('chai').assert;
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
+const IsNumeric = (n) => {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+
+describe('Curse', () => {
+    const curse = require('../lib/curse.js');
+    describe('getDownloadUrl()', () => {
+        it('should return the download url of an addon', (done) => {
+            curse.getDownloadURL('Ace3', null, (err, url, version) => {
+                should.not.exist(err);
+                should.exist(url);
+                should.exist(version);
+                const zipRegex = /\.zip/;
+                let hasZip = zipRegex.exec(url);
+
+                should.not.equal(null, hasZip);
+                should.not.equal(null, version);
+
+                IsNumeric(version).should.equal(true);
+                done()
+            }) 
+        })
+    })
 });
+
+describe('downloader', () => {
+    const curse = require('../lib/curse.js');
+    const downloader = require('../downloader.js');
+    describe('getDownloadUrl()', () => {
+        it('should return the download url of an addon', (done) => {
+            curse.getDownloadURL('Ace3', null, (err, url, version) => {
+                should.not.exist(err);
+                should.exist(url);
+                should.exist(version);
+                const zipRegex = /\.zip/;
+                let hasZip = zipRegex.exec(url);
+
+                should.not.equal(null, hasZip);
+                should.not.equal(null, version);
+
+                IsNumeric(version).should.equal(true);
+                done()
+            }) 
+        })
+    })
+});
+
