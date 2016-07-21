705cc37c8a6cbc7a1ddc2dc054bfd8818917a692
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2016-07-21 15:47:07 +0200

    prompt user for wow folder

--- cli.js
+++ cli.js
@@ -5,46 +5,165 @@ const log = require('npmlog');
 const path = require('path');
 const cli = require('cli');
 
-const downloader = require('./lib/downloader');
+const Save = require('./lib/save.js');
+const Wow = require('./lib/wow.js');
 
 log.addLevel('cli', 3000, { fg: 'cyan' }, 'CLI');
-cli.parse(null, ['install', 'update', 'checkupdate', 'uninstall', 'installed', 'ls']);
-cli.main(function (args, options) {
-    let context = findWowDir()
-    console.log(args)
-    console.log(options)
+const DEFAULT_PLATFORM = 'curse';
 
-    switch (cli.command) {
-      case 'install': {
-        install(context)
-        break;
-      }
-    }
-})
+let parseOptions = {
+    platform: [ 'p', 'Select the platform of the addon.', 'string', DEFAULT_PLATFORM],          // -f, --file FILE   A file to process
+    version: [ 'v', 'Install a specific version of the addon.', 'int', false],                 // -t, --time TIME   An access time
+}
 
-function findWowDir() {
-  //Figure out the user's WoW install directory
-  var wowdir;
-  if (process.env.WOWPATH) {
-    wowdir = process.env.WOWPATH;
-  } else {
-    log.warn('path', 'Using default WoW path, please set the WOWPATH env variable');
-    wowdir = path.join('C:', 'Program Files (x86)', 'World Of Warcraft');
-  }
+if (true) {
+  parseOptions.debug = ['d', 'Debug flag', false, true];
+}
 
-  log.cli('wowDir', wowdir);
+cli.parse(
+    parseOptions,
+    ['install', 'update', 'checkupdate', 'uninstall', 'installed', 'ls', 'changewow']
+);
 
-  var addonsdir = path.join(wowdir, 'Interface', 'AddOns');
-  var savefile = path.join(wowdir, '.addons.json');
-  return {
-    wow: wowdir,
-    addons: addonsdir,
-    savefile: savefile
-  }
+const commands = {
+  install: install,
+  changewow: changewow,
+
 }
 
 
-function install(ctx) {
+cli.main(function (args, options) {
+    console.log(args)
+    console.log(options)
+    findWowDir(options, (err, wow) => {
+      let commandHandler = commands[cli.command];
+      commandHandler(wow, args, options);
+    })
+})
+
+function promptWowDir(configFile, data, cb) {
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const DEFAULTS_WOWDIR = {
+    '1': path.join('C:', 'Program Files', 'World Of Warcraft'),
+    '2': path.join('C:', 'Program Files (x86)', 'World Of Warcraft')
+  }
+
+  console.log('Where is your World of Warcraft folder ?')
+  console.log('  1. ' + DEFAULTS_WOWDIR['1'])
+  console.log('  2. ' + DEFAULTS_WOWDIR['2'])
+  console.log('  3. Other')
+
+  rl.question('', (answer) => {
+    if (answer === '3') {
+      rl.question('Enter the path pls\n', (answer) => {
+        log.cli('wowdir answer', 'should be in ' + answer);
+        handleAnswer(answer)
+        rl.close();
+      })
+    } else {
+      handleAnswer(DEFAULTS_WOWDIR[answer])
+      rl.close();
+    }
+  })
+
+  let handleAnswer = (presumedWowdir) => {
+    if (!presumedWowdir) {
+      throw 'error wrong directory specified';
+    }
+    let newData = data || {};
+    newData.wowdir = presumedWowdir;
+    configFile.write(newData, (err) => {
+      return cb(newData)
+    })
+  }
+
+}
+
+function findWowDir(options, cb) {
+  //Figure out the user's WoW install directory
+  var wowdir;
+
+  if (options && options.debug) {
+    const util = require('./lib/util.js');
+    return util.makeTmpWowFolder((err, tmpwowpath) => {
+      if (err) {
+        return cb(err)
+      }
+
+      return cb(null, new Wow(tmpwowpath));
+    })
+  }
+
+  let wowFoundCallback = (data) => {
+    log.cli('wowDir', data.wowdir);
+    cb(null, new Wow(data.wowdir));
+  }
+
+  let configFile = new Save();
+  configFile.read((err, data) => {
+    if (!data || !data.wowdir) {
+      // prompt user
+      log.cli('shouldprompt', 'hi');
+
+      promptWowDir(configFile, data, wowFoundCallback);
+
+    } else {
+      return wowFoundCallback(data);
+    }
+  })
+}
+
+
+function install(wow, args, options) {
   log.cli('install', 'install cmd')
 
+  if (args.length < 1) {
+    // error message
+    log.cli('error', 'Should specify addon as argument')
+    return
+  }
+
+  let addonName = args[0];
+  let platform = options.platform;
+  let version = options.version ? options.version : null;
+
+  log.cli('install', `try to install ${addonName} with platform ${platform} and version ${version}`);
+  if (!wow.isPlatformValid(platform)) {
+    // error message
+    log.cli('error', `platform ${platform} is invalid, platform is set to default '${DEFAULT_PLATFORM}'`);
+    platform = DEFAULT_PLATFORM;
+  }
+
+  wow.install(platform, addonName, version, (err) => {
+    if (err) {
+        log.cli('error', err);
+        // throw err;
+    }
+  })
+}
+
+function changewow(wow, args, options) {
+  wow.saveFd.read((err, data) => {
+    if (err) {
+      throw err;
+    }
+    data.wowdir = undefined;
+
+    if (args < 1) {
+      promptWowDir(wow.saveFd, data, (data) => {
+        log.cli('changewowdir', 'new dir is ' + data.wowdir)
+      })
+    } else {
+      // user specified wowdir in the commandline
+      data.wowdir = args[0];
+      wow.saveFd.write(data, (err) => {
+        log.cli('changewowdir', 'new dir is ' + data.wowdir)
+      })
+    }
+  })
 }
--- tests/test.js
+++ tests/test.js
@@ -15,29 +15,13 @@ const IsNumeric = (n) => {
 
 const curse = require('../lib/curse.js');
 const downloader = require('../lib/downloader.js');
-const Wow = require('../lib/index.js');
+const Wow = require('../lib/wow.js');
 
+const utils = require('../lib/util.js');
 
-const testTimeout = 20*1000;
+const testTimeout = 30*1000;
 
-const makeTmpWowFolder = (cb) => {
-    temp.mkdir('wowfolder', function(err, wowFolder) {
-        if (err) {
-            return cb(err)
-        }
-
-        let interfaceDir = path.join(wowFolder, 'Interface')
-        mkdirp(interfaceDir, (err) => {
-            should.not.exist(err)
-            let addonsDir = path.join(interfaceDir, 'AddOns')
-            mkdirp(addonsDir, (err) => {
-                should.not.exist(err)
-
-                cb(null, wowFolder);
-            })
-        })
-    })
-}
+const makeTmpWowFolder = utils.makeTmpWowFolder;
 
 describe('Curse', function() {
     describe('getDownloadUrl()', function() {
@@ -65,7 +49,7 @@ describe('Curse', function() {
                 should.not.exist(err);
                 should.exist(wowPath);
 
-                let wow = new Wow(wowPath);
+                let wow = new Wow(wowPath, wowPath);
                 let mockAddonData = {
                     addons: {
                         'Ace3': {
@@ -81,6 +65,71 @@ describe('Curse', function() {
                         should.not.exist(err);
                         isNew.should.equal(true);
                         done()
+                    })
+                })
+            })
+        })
+
+        it('should tell addon version is up to date (be sure to update the number ...)', function(done) {
+            this.timeout(testTimeout)
+            makeTmpWowFolder(function(err, wowPath) {
+                should.not.exist(err);
+                should.exist(wowPath);
+
+                let wow = new Wow(wowPath, wowPath);
+                let mockAddonData = {
+                    addons: {
+                        'Ace3': {
+                            platform: 'curse',
+                            version: 924908,
+                            folders: null
+                        }
+                    }
+                }
+                wow.saveFd.write(mockAddonData, (err) => {
+                    should.not.exist(err);
+                    wow.checkupdate('Ace3', (err, isNew, platform, zipUrl, version) => {
+                        should.not.exist(err);
+                        isNew.should.equal(false);
+                        done()
+                    })
+                })
+            })
+        })
+
+
+        it('should tell the new version of multiple addons', function(done) {
+            this.timeout(testTimeout)
+            makeTmpWowFolder(function(err, wowPath) {
+                should.not.exist(err);
+                should.exist(wowPath);
+
+                let wow = new Wow(wowPath, wowPath);
+                let mockAddonData = {
+                    addons: {
+                        'Ace3': {
+                            platform: 'curse',
+                            version: 924908,
+                            folders: null
+                        },
+                        'Auctionator': {
+                            platform: 'curse',
+                            version: 0,
+                            folders: null
+                        },
+                        'Bagnon': {
+                            platform: 'curse',
+                            version: 0,
+                            folders: null
+                        }
+                    }
+                }
+                wow.saveFd.write(mockAddonData, (err) => {
+                    should.not.exist(err);
+                    wow.checkAllAddonsForUpdate([], (err, addonsToUpdate) => {
+                        should.not.exist(err);
+                        addonsToUpdate.length.should.equal(2);
+                        done();
                     })
                 })
             })
@@ -114,7 +163,7 @@ describe('download wow addon into wow folder', function() {
                 should.not.exist(err);
                 should.exist(wowPath);
 
-                let wow = new Wow(wowPath);
+                let wow = new Wow(wowPath, wowPath);
                 wow.install('curse', 'Ace3', null, (err) => {
                     should.not.exist(err);
                     fs.access(wow.getSaveFile(), fs.constants.R_OK | fs.constants.W_OK, (err) => {
@@ -139,7 +188,7 @@ describe('download wow addon into wow folder', function() {
             }
 
             makeTmpWowFolder(function(err, wowPath) {
-                let wow = new Wow(wowPath);
+                let wow = new Wow(wowPath, wowPath);
                 wow.install('curse', 'Ace3', null, (err) => {
                     fs.access(wow.getSaveFile(), fs.constants.R_OK | fs.constants.W_OK, (err) => {
                         should.not.exist(err);
